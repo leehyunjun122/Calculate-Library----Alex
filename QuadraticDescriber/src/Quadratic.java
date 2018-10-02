c95f42f599df3822a90283aa54c12b0301d8ff75
APCS3rd <APCS3rd@MHS-Math2-33>
2018-10-02 10:56:12 -0700

    hi

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,4 +1,7 @@
-
+/*This class contain methods that can perform various math operations that are related to Quadratics. 
+ * @author Alex Lee
+ * @versions October 2, 2018
+ */
 public class Quadratic {
 
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,4 +1,8 @@
-
+/*This class contain main methods that perform various math operations 
+ * that handles interactions with the user.
+ * @author Alex Lee
+ * @versions October 2, 2018
+ */
 public class QuadraticClient {
 
 }
